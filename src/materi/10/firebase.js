905f77a0f38b9273659ca145c4da916538352410
Aisy Rozsidhy <aisymuhammad@gmail.com>
2020-09-28 16:35:11 +0700

    update

--- src/materi/10/firebase.js
+++ src/materi/10/firebase.js
@@ -2,13 +2,13 @@ import Firebase from 'firebase';
 import "firebase/firestore";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyAgTk7hS6LJDKGWK08T85ebbKM3ckOXm48",
-  authDomain: "coba2-d7462.firebaseapp.com",
-  databaseURL: "https://coba2-d7462.firebaseio.com",
-  projectId: "coba2-d7462",
-  storageBucket: "coba2-d7462.appspot.com",
-  messagingSenderId: "822412515173",
-  appId: "1:822412515173:web:30cb867d21939ade0db38c"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 Firebase.initializeApp(firebaseConfig);
